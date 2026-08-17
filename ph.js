4e70e81b67d4e67be496a33b440d358bb334aea8
koemrachhi1122 <koemrachhi1122@gmail.com>
2026-08-17 19:05:06 +0700

    Add files via upload

---
+++ ph.js
@@ -0,0 +1,25 @@
+// =========================
+// PhoneShop JavaScript
+// =========================
+
+
+// Confirm Delete
+
+function confirmDelete(){
+
+    return confirm(
+        "Are you sure you want to delete this item?"
+    );
+
+}
+
+
+// Add cart message
+
+function addCart(){
+
+    alert(
+        "Product added to cart!"
+    );
+
+}
